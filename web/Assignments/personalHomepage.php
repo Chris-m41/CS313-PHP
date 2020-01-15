018a74b0edcb46132349af9c6f8f942fdbc2debe
Chris-m41 <Chrism577@yahoo.com>
2020-01-15 13:25:12 -0700

    Introducing php files

---
+++ web/Assignments/personalHomepage.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Home Page</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="personalHomepage.css">
+</head>
+
+<body>
+    <div id="title">
+        <h1>Welcome to my webpage</h1>
+        <h2>Christian Martinez</h2>
+    </div>
+    <div class="container">
+        <h2>About Me</h2>
+        <div class="row">
+            <div class="col-sm-6">
+                <h5>Interests</h5>
+                <ul>
+                    <li>Video Games</li>
+                    <li>Pizza</li>
+                    <li>Sleep</li>
+                    <li>TV</li>
+                    <li>Music</li>
+                </ul>
+            </div>
+            <div class="col-sm-6">
+                <h5>Bio</h5>
+                <p>I am a Software Engineering major with two semesters left. I have studied web development since I was 16 years old. I am currently aspiring to become a mobile application developer.</p>
+            </div>
+
+        </div>
+        <h5>Resume</h5>
+        <div class="col-lg-12">
+            <img src="Resume.png" alt="">
+        </div>
+        <a href="personalHomepage2.html">See my CS 313 Work!</a>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
