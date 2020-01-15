0a1432b6015b708008c6ba3af1453d663865957b
Chris-m41 <Chrism577@yahoo.com>
2020-01-15 13:39:42 -0700

    Added footer to webpages

--- web/Assignments/personalHomepage2.php
+++ web/Assignments/personalHomepage2.php
@@ -14,6 +14,8 @@
 <body>
     <h1>Check out my personal work!</h1>
     <h4>Coming soon!</h4>
+
+    <?php include 'index.php';?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
