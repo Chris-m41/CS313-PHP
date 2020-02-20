d8c0aeed2dd6135ce5e4d53411656618d7abad4f
Chris <chrism577@yahoo.com>
2020-02-20 11:58:39 -0700

    fixed database

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,29 +1,32 @@
 CREATE TABLE Cookies (
       id SERIAL NOT NULL PRIMARY KEY
     , display_name VARCHAR(30)
-    , price NUMBER
+    , price NUMERIC (5,2)
 );
 
 CREATE TABLE Soda (
       id SERIAL NOT NULL PRIMARY KEY
     , display_name VARCHAR(30)
-    , price NUMBER
+    , price NUMERIC (5,2)
+    , yes_no BIT
 );
 
 CREATE TABLE Soaps (
       id SERIAL NOT NULL PRIMARY KEY
     , display_name VARCHAR(30)
-    , price NUMBER
+    , price NUMERIC (5,2)
 );
 
 CREATE TABLE Chocolate (
       id SERIAL NOT NULL PRIMARY KEY
     , display_name VARCHAR(30)
-    , price NUMBER
+    , price NUMERIC (5,2)
+    , ounces NUMERIC (5,2)
 );
 
 CREATE TABLE Gum (
     id SERIAL NOT NULL PRIMARY KEY
     , display_name VARCHAR(30)
     , price NUMBER
+    , pieces INTEGER
 );
