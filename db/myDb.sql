712fe853e27f81c43e029c4e53a0935e9e7ef7f8
Chris <chrism577@yahoo.com>
2020-02-20 12:17:48 -0700

    working with heroku

--- db/myDb.sql
+++ db/myDb.sql
@@ -28,10 +28,6 @@ CREATE TABLE Gum (
     id SERIAL NOT NULL PRIMARY KEY
     , display_name VARCHAR(30)
     , price NUMBER
-<<<<<<< HEAD
-    , pieces INTEGER
-);
-=======
 );
 
 CREATE TABLE scriptures
@@ -51,4 +47,3 @@ INSERT INTO scriptures VALUES (nextval('scriptures_s1'), 'John', 1, 5, 'And the 
 INSERT INTO scriptures VALUES (nextval('scriptures_s1'), 'Doctrine and Covenants', 88, 49, 'The light shineth in darkness, and the darkness comprehendeth it not; nevertheless, the day shall come when you shall comprehend even God, being quickened in him and by him.');
 INSERT INTO scriptures VALUES (nextval('scriptures_s1'), 'Mosiah', 16, 9, 'He is the light and the life of the world; yea, a light that is endless, that can never be darkened; yea, and also a life which is endless, that there can be no more death.');
 INSERT INTO scriptures VALUES (nextval('scriptures_s1'), 'Doctrine and Covenants', 93, 28, 'He that keepeth his commandments receiveth truth and light, until he is glorified in truth and knoweth all things.');
->>>>>>> 0c8d221489421642d223464c6fa9ed527e0a4619
