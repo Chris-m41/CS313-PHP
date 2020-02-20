10aa161fa2b75e99b6012a8141801f252a92c80d
Chris <chrism577@yahoo.com>
2020-02-20 13:41:26 -0700

    working with heroku

--- web/Assignments/week05/details.php
+++ web/Assignments/week05/details.php
@@ -1,67 +1,31 @@
-<?php
-/**********************************************************
-* File: viewScriptures.php
-* Author: Br. Burton
-* 
-* Description: This file shows an example of how to query a
-*   PostgreSQL database from PHP.
-***********************************************************/
-
-require "connect.php";
-$db = get_db();
-
-?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Scripture List</title>
+    <?php
+        require('connect.php');
+        $db = get_db();
+
+        $query = 'SELECT id, display_name, price FROM Cookies';
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+        $courses = $stmt->fetchAll(PDO:FETCH_ASSOC);
+    ?>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Details</title>
 </head>
-
 <body>
-<div>
-
-<h1>Scripture Resources</h1>
-
-<?php
-
-// In this example, for simplicity, the query is executed
-// right here and the data echoed out as we iterate the query.
-
-// You could imagine that in a more involved application, we
-// would likely query the database in a completely separate file / function
-// and build a list of objects that held the components of each
-// scripture. Then, here on the page, we could simply call that 
-// function, and iterate through the list that was returned and
-// print each component.
-
-
-
-// First, prepare the statement
-
-// Notice that we avoid using "SELECT *" here. This is considered
-// good practice so we don't inadvertently bring back data we don't
-// want, especially if the database changes later.
-$statement = $db->prepare("SELECT book, chapter, verse, content FROM scriptures");
-$statement->execute();
-
-// Go through each result
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
-	// The variable "row" now holds the complete record for that
-	// row, and we can access the different values based on their
-	// name
-	$book = $row['book'];
-	$chapter = $row['chapter'];
-	$verse = $row['verse'];
-	$content = $row['content'];
-
-	echo "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p>";
-}
-
-?>
-
-
-</div>
-
+    <?php
+    foreach($courses as $course)
+    {
+        $id = $course['id'];
+        $name = $course['name'];
+        $price = $course['price'];
+        echo"<li><p>$name - $price </p></li>"
+    }
+        echo '<p><b>' . $result['display_name'] . ':' . $result['price'] . '</b> - "';
+    ?>
 </body>
 </html>
