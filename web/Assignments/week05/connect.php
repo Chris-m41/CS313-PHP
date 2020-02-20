c1d4f21d165f4eb1d53a6a0189bdd42455e65b36
Chris <chrism577@yahoo.com>
2020-02-20 13:03:18 -0700

    working with heroku

--- web/Assignments/week05/connect.php
+++ web/Assignments/week05/connect.php
@@ -30,6 +30,13 @@
             echo ' password: ' . $row['password'];
             echo '<br/>';
             }
+        $statement = $db->query('SELECT username, password FROM note_user');
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+        {
+            echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
+        }
+        $statement = $db->query('SELECT username, password FROM note_user');
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
     ?>
 
     <meta charset="UTF-8">
