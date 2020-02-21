0decad5249fe471fb2511adf0ddaee00c6a716a0
Chris <chrism577@yahoo.com>
2020-02-21 15:12:57 -0700

    working with heroku

---
+++ web/Assignments/week06/phpRedirects/chocolate.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Add Soap</title>
+</head>
+<body>
+<form action="../forms/addChocolate.php" method="post">
+    <textarea name="display_name"></textarea>
+    <textarea name="price"></textarea>
+    <textarea name="ounces"></textarea>
+    <input type="submit" value="Add Soap Info" />
+    </form>
+
+
+    <h3><a href="../details.php">Return to Main Menu</a></h3>
+</body>
+</html>
