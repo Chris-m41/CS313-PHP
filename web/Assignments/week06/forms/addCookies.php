b8f5b0a28373ada31fb0ecde6b66a22a978810d8
Chris <chrism577@yahoo.com>
2020-02-21 12:40:35 -0700

    working with heroku

--- web/Assignments/week06/forms/addCookies.php
+++ web/Assignments/week06/forms/addCookies.php
@@ -7,39 +7,64 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="../details.php" method="post">
+    <form action="addCookies.php" method="post">
     <textarea name="display_name"></textarea>
     <textarea name="price"></textarea>
     <input type="submit" value="Add Cookie Info" />
     </form>
+
     <?php
-        require('connect.php');
+        // get the data from the POST
+    $display_name = $_POST['display_name'];
+    $price = $_POST['price'];
+  
+// For debugging purposes, you might include some echo statements like this
+// and then not automatically redirect until you have everything working.
 
-        $db = get_db();
+// echo "book=$book\n";
+// echo "chapter=$chapter\n";
+// echo "verse=$verse\n";
+// echo "content=$content\n";
 
-        if(isset($_POST["submit"])){
-           
-            try {
-            $dbh = $db
-            
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // <== add this line
-            $sql = "INSERT INTO students (student_name, student_email, student_city)
-            VALUES ('".$_POST["display_name"]."','".$_POST["price"]'");
-            if ($db->query($sql)) {
-            echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
-            }
-            else{
-            echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
-            }
-            
-            $db = null;
-            }
-            catch(PDOException $e)
-            {
-            echo $e->getMessage();
-            }
-            
-            }
+// we could (and should!) put additional checks here to verify that all this data is actually provided
+
+
+require("dbConnect.php");
+$db = get_db();
+
+try
+{
+	// Add the Scripture
+
+	// We do this by preparing the query with placeholder values
+	$query = 'INSERT INTO cookies(display_name, price) VALUES(:display_name, :price )';
+	$statement = $db->prepare($query);
+
+	// Now we bind the values to the placeholders. This does some nice things
+	// including sanitizing the input with regard to sql commands.
+	$statement->bindValue(':display_name', $display_name);
+	$statement->bindValue(':price', $price);
+
+	$statement->execute();
+
+	// get the new id
+	$cookieId= $db->lastInsertId("id");
+}
+catch (Exception $ex)
+{
+	// Please be aware that you don't want to output the Exception message in
+	// a production environment
+	echo "Error with DB. Details: $ex";
+	die();
+}
+
+// finally, redirect them to a new page to actually show the topics
+header("Location: showTopics.php");
+
+die(); // we always include a die after redirects. In this case, there would be no
+       // harm if the user got the rest of the page, because there is nothing else
+       // but in general, there could be things after here that we don't want them
+       // to see.
     ?>
 </body>
 </html>
