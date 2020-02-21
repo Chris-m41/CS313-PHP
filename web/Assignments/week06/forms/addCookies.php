4500f3cc216a1f422b2ddf39fa38093cb360c325
Chris <chrism577@yahoo.com>
2020-02-21 13:18:57 -0700

    working with heroku

--- web/Assignments/week06/forms/addCookies.php
+++ web/Assignments/week06/forms/addCookies.php
@@ -51,7 +51,7 @@
     }
 
     // finally, redirect them to a new page to actually show the topics
-    header("Location: ../details.php");
+    // header("Location: ../details.php");
 
     die(); // we always include a die after redirects. In this case, there would be no
         // harm if the user got the rest of the page, because there is nothing else
