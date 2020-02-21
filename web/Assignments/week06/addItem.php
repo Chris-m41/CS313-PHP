b235af38529deb1b0b3f976b130ecac849bee89e
Chris <chrism577@yahoo.com>
2020-02-20 17:01:45 -0700

    working with heroku

--- web/Assignments/week06/addItem.php
+++ web/Assignments/week06/addItem.php
@@ -9,5 +9,24 @@
 <body>
     <h2>Want to add an item to the list?</h2>
     <h2>Click on the item and then type in the required information.</h2>
+    <form action="#" method="post">
+        <input type="checkbox" name="Cookies" value="Cookies">Cookies</input>
+        <input type="checkbox" name="Soda" value="Soda">Soda</input>
+        <input type="checkbox" name="Soap" value="Soap">Soap</input>
+        <input type="checkbox" name="Chocolate" value="Chocolate">Chocolate</input>
+        <input type="checkbox" name="Gum" value="Gum">Gum</input>
+        <input type="submit" name="submit" value="Submit"/>
+    </form>
+
+   <?php
+    if (isset($_POST['Cookies'])){
+        echo $_POST['Cookies'];
+        <form action="#" method="post">
+            <textarea name="display_name"></textarea>
+            <textarea name="price"></textarea>
+        </form>
+        <input type="submit" name="submit" value="Submit"/>
+    }
+   ?>
 </body>
 </html>
