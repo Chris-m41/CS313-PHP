718cf281ea4e71a6a7d75864cee6fd32cb683376
Chris <chrism577@yahoo.com>
2020-02-21 13:23:05 -0700

    working with heroku

--- web/Assignments/week06/forms/addCookies.php
+++ web/Assignments/week06/forms/addCookies.php
@@ -7,7 +7,7 @@
     <title>Add Cookies</title>
 </head>
 <body>
-    <form action="addCookies.php" method="post">
+    <form action="details.php" method="post">
     <textarea name="display_name"></textarea>
     <textarea name="price"></textarea>
     <input type="submit" value="Add Cookie Info" />
