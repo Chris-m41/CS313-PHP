1e6dbdb8fa31506d199dc5e39296b4c8a1796a5f
Chris <chrism577@yahoo.com>
2020-02-21 18:24:53 -0700

    Adding Styling

--- web/Assignments/week06/phpRedirects/chocolate.php
+++ web/Assignments/week06/phpRedirects/chocolate.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="../index.css">
     <title>Add Chocolate</title>
 </head>
 <body>
@@ -18,5 +18,5 @@
 
 
     <h3><a href="../details.php">Return to Main Menu</a></h3>
-</body>
+    </body>
 </html>
