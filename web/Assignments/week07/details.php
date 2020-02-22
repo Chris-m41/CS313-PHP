1e093607e079dd42400fbf7ee6c299507a52fba5
Chris <chrism577@yahoo.com>
2020-02-21 19:45:51 -0700

    Adding Styling

--- web/Assignments/week07/details.php
+++ web/Assignments/week07/details.php
@@ -116,6 +116,7 @@
             }
         }
     ?>
+    <br>
      <h3><a href="addItem.php">Add an Item</a></h3>
      </div>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
