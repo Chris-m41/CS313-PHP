a700df375c387b93ad7ae114f675ee4991c63c4e
Chris <chrism577@yahoo.com>
2020-02-21 17:26:18 -0700

    Adding Styling

--- web/Assignments/week06/details.php
+++ web/Assignments/week06/details.php
@@ -41,13 +41,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="index.css">
     <title>Details</title>
 </head>
 <body>
-  
+  <div class=".contianer">
+        <div class=".container-md">
     <h1>See brands pertaining to that item.</h1>
     <h3>Click on one checkbox at a time</h3>
+        </div>
+        <div class=".container-md">
     <form action="#" method="post">
         <input type="checkbox" name="Cookies" value="Cookies">Cookies</input>
         <input type="checkbox" name="Soda" value="Soda">Soda</input>
@@ -56,6 +59,9 @@
         <input type="checkbox" name="Gum" value="Gum">Gum</input>
         <input type="submit" name="submit" value="Submit"/>
     </form>
+    </div>
+    <h3><a href="addItem.php">Add an Item</a></h3>
+    </div>
     <?php
         if (isset($_POST['Cookies'])){
             echo $_POST['Cookies']; // Displays value of checked checkbox.
@@ -112,7 +118,6 @@
             }
         }
     ?>
-    <h3><a href="addItem.php">Add an Item</a></h3>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
