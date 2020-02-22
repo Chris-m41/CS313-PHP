e39904ed629d409eaa01a06dbfb0140a24ad5466
Chris <chrism577@yahoo.com>
2020-02-21 17:20:07 -0700

    Adding Styling

---
+++ web/Assignments/week07/addItem.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AddItem</title>
+</head>
+<body>
+    <h2>Want to add an item to the list?</h2>
+    <h2>Click on the item and then type in the required information.</h2>
+        <form action="phpRedirects/cookies.php">
+        <input type="submit" value="Add Cookie Brand" />
+        </form>
+
+        <form action="phpRedirects/soda.php">
+        <input type="submit" value="Add Soda Brand" />
+        </form>
+
+        <form action="phpRedirects/soap.php">
+        <input type="submit" value="Add Soap Brand" />
+        </form>
+
+        <form action="phpRedirects/chocolate.php">
+        <input type="submit" value="Add Chocolate Brand" />
+        </form>
+
+        <form action="phpRedirects/gum.php">
+        <input type="submit" value="Add Gum Brand" />
+        </form>
+    
+</body>
+</html>
